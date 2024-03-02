5ee50b88fdccc7825643be0c9182f8741da49faf
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-01 21:28:54 -0600

    added new folder

--- JavaScript/dataTypes/1.NumberDataType/01.Integers.js
+++ JavaScript/dataTypes/1.NumberDataType/01.Integers.js
@@ -1,5 +1,6 @@
 //Number:  Unlike other programming languages, you don’t need int, float, etc to declare different numeric values.
 // You can defined any numbers here.
+//In which can handle both INTEGER and Floating-point (decimal) values.
 
 //Note: you can apply any method on any numbers.
 let number1 = 45;
@@ -12,4 +13,14 @@ console.log(`Multiplication = ` + multi);
 console.log(`Division = ` + div);
 //Note you can also use the function or method dirctly in the console
 console.log(`Modulo is = ` + number1%number2);
+
+
+// Example 2
+let x = 89;
+let y = 77;
+let divideByTwo = (x+y)/2;
+console.log(divideByTwo);
+
+//Example 3
+
 
