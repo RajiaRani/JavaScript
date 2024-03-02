be6defa6562d91f83ed8ca2a937a0cbb19a28700
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-02 14:55:47 -0600

    added new programs

--- JavaScript/AssignmentOperators/01.Basic .js
+++ JavaScript/AssignmentOperators/01.Basic .js
@@ -4,21 +4,3 @@ console.log(a); // Output: 10
 const x = 3.14;
 console.log(x);
 
-
-
-//Example 1
-let age = 29;
- age = age + 3;
- console.log(age);
-
-
-//Example 2
-let price = 56;
-price = price - 5;
-console.log('price = ', price);
-
-
-//Example 3
-let marks = 67;
-marks = marks * 2;
-console.log('marks = ', marks);
