68b5b5946a7fde873428d6feab6ceb7bd303b33c
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-01 20:50:17 -0600

    added

---
+++ JavaScript/01.variables..js
@@ -0,0 +1,5 @@
+//Variables are Containers for Storing Data
+let a = 8; // a stores the value 8
+let b = 90; // b stores the value 90
+let sum = a+b; // sum stores the value 8+90=98
+console.log(`sum = `+  sum);
