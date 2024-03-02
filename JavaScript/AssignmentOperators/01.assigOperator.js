f5d284dbc9a7ead7ccda5bdd6356c976ec1de3bc
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-02 14:48:48 -0600

    added

---
+++ JavaScript/AssignmentOperators/01.assigOperator.js
@@ -0,0 +1,16 @@
+//Example 1
+let age = 29;
+ age = age + 3;
+ console.log(age);
+
+
+//Example 2
+let price = 56;
+price = price - 5;
+console.log('price = ', price);
+
+
+//Example 3
+let marks = 67;
+marks = marks * 2;
+console.log('marks = ', marks);
