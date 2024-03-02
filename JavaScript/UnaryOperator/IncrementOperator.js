0fd1c5c24a7f7b0156c44285ba72713c2edd2e64
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-02 15:03:08 -0600

    added

---
+++ JavaScript/UnaryOperator/IncrementOperator.js
@@ -0,0 +1,16 @@
+//Example 1
+let e = 5;
+console.log(++e); // Output: 6 (prefix increments then returns the value)
+let f = 5;
+console.log(f++); // Output: 5 (postfix returns the value then increments)
+console.log(f); // Output: 6
+
+//Exapmle 2
+let year = 2024;
+console.log("First increment = ", ++year);
+
+//Example 3
+let age = 23;
+console.log("No increment = ", age++);
+console.log("Postfic = ",age);
+console.log("after increment = ", ++age);
