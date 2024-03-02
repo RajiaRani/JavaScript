348103641d62bd013ed14be71cc513911cd62d45
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-02 14:15:47 -0600

    added new program

--- JavaScript/dataTypes/1.NumberDataType/02.float.js
+++ JavaScript/dataTypes/1.NumberDataType/02.float.js
@@ -1,5 +1,33 @@
 // An example of a floating-point number
+
+//Exapmle 1
 let floatingPointNumber = 3.14; 
 let circle = 2*floatingPointNumber;
 console.log(`Floating Number =` + circle);
 
+//Exapmle 2
+let num = 99.9;
+let num2 = 89.5;
+let num3 =20.8;
+let sum = num + num2 + num3;
+console.log('sum is = ', sum);
+
+//Example 3
+//find the distance
+let velocity = 67.89;
+let time = 60;
+let distance = velocity*time;
+console.log('The distance is = ', distance);
+
+//Example 4
+//find the diameter of the circle
+//let we have radius value and pi value
+let radius = 7.8;
+const  pii = 3.14;
+let diameter = 2*radius;
+console.log('Diameter is = ', diameter);
+
+
+//formula for the circumference is  C = 2*pi*r  OR C=pi*diameter
+let circumference = pii * diameter;
+console.log('The circumference is = ', circumference);
