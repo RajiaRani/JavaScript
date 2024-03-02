5cd8e2cdc16659d8ac749c7a67d0e3c95e8b70ae
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-02 14:38:54 -0600

    added

---
+++ JavaScript/dataTypes/3.String/01.str.js
@@ -0,0 +1,7 @@
+
+let string = "hello my name is Rajia Rani";
+console.log("The string = ", string);
+
+//you can also put the string directly inside the console
+console.log("Hello, everyone , I am Rahul Rajput. i did master in Chemistry.");
+
