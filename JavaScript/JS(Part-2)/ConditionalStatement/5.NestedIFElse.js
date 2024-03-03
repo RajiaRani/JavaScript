7b793ffa4ace81b18f79c4695ca48a0984e95810
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-03 13:00:21 -0600

    added

--- JavaScript/JS(Part-2)/ConditionalStatement/5.NestedIFElse.js
+++ JavaScript/JS(Part-2)/ConditionalStatement/5.NestedIFElse.js
@@ -17,17 +17,17 @@ if (marks >= 33) {
 
 //Example 2
 let age = 20;
-if(age >= 18){
+if (age >= 18) {
     console.log("You are eligible");
-    if(age >= 21){
+    if (age >= 21) {
         console.log("you have a right to get marry");
-    } else if( age >=19){
+    } else if (age >= 19) {
         console.log("You can Vote");
-    }else{
+    } else {
         console.log("According to law you are under age, then you cant get marry under 21. ");
     }
-    
-} else{
+
+} else {
     console.log("You are under age");
 }
 
@@ -37,31 +37,64 @@ let timeOfDay = 18; // 24-hour format
 let isRaining = true;
 
 if (weather === "cold") {
-  console.log("Wear a jacket.");
+    console.log("Wear a jacket.");
 
-  if (isRaining) {
-    console.log("Don't forget an umbrella!");
-  }
+    if (isRaining) {
+        console.log("Don't forget an umbrella!");
+    }
 } else if (weather === "warm") {
-  if (timeOfDay < 19) {
-    console.log("A t-shirt should be fine.");
-  } else {
-    console.log("It's warm, but you might need a light sweater in the evening.");
-  }
+    if (timeOfDay < 19) {
+        console.log("A t-shirt should be fine.");
+    } else {
+        console.log("It's warm, but you might need a light sweater in the evening.");
+    }
 } else {
-  console.log("Weather condition unknown. Dress comfortably.");
+    console.log("Weather condition unknown. Dress comfortably.");
 }
 
 
 //Example 4
 attedence = 78;
-if(attedence >=75){
+if (attedence >= 75) {
     console.log("You are eligible to attend the exam");
-    if(attedence >= 95){
+    if (attedence >= 95) {
         console.log("You got the extra 5 marks");
-    } else{
+    } else {
         console.log("You got the 2 marks");
     }
-} else{
+} else {
     console.log("Sorry! your attend is too short");
+}
+
+//Example 5
+let score = 88;
+if (score >= 50) {
+    console.log("Pass");
+    if (score >= 95) {
+        console.log("A+");
+    } else if (score >= 80) {
+        console.log("B");
+        if (score >= 85) {
+            console.log("B+");
+        } else {
+            console.log("B-");
+        }
+    } else if (score >= 70) {
+        console.log("C");
+        if (score >= 75) {
+            console.log("C+");
+        } else {
+            console.log("C-");
+        }
+    } else if (score >= 60) {
+        console.log("D");
+        if (score >= 65) {
+            console.log("D+");
+        } else {
+            console.log("D-");
+        }
+    }
+}
+else {
+    console.log("F");
 }
