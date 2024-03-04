d4acd707049d13065e301f68ba9a93769c1c6644
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-03 23:55:58 -0600

    added slice

---
+++ JavaScript/JS(Part-3)/ArrayDataStructure/9.Slice.js
@@ -0,0 +1,6 @@
+let colors = [ 'red', 'green', 'orange', 'blue', 'pink', 'black', 'yellow'];
+let newColors = colors.slice(3);
+console.log(newColors);
+console.log(colors.slice()); // print the original array
+console.log(colors.slice(2,4));
+console.log(colors.slice(-3));
