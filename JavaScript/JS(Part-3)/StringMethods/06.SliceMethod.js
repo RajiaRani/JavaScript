b018832ab5d464d9e581a2649d662b880f8c00a3
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-03 22:32:34 -0600

    added

---
+++ JavaScript/JS(Part-3)/StringMethods/06.SliceMethod.js
@@ -0,0 +1,11 @@
+// Slice Method:
+// Returns a part of the original string as a new String.
+
+let str = "ILoveCoding";
+let newStr = str.slice(1,5); // passing the starting index and (ending index+1)
+console.log(newStr);
+
+
+let str2 = "I love my India";
+let newStr2 = str2.slice(5);
+console.log(newStr2);
