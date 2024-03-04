962b9b89526fd4789dba39d57c00273f20ed8223
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-04 00:21:46 -0600

    updated

--- JavaScript/JS(Part-3)/ArrayDataStructure/11.Sort.js
+++ JavaScript/JS(Part-3)/ArrayDataStructure/11.Sort.js
@@ -5,6 +5,8 @@ console.log("Sort the Array : ", days.sort());
 
 
 // Example 2
+// Sort is not good for numbers.
 let num = [ 78, 8, 6, 4, 23, 11 , 56, 33, 90.9, 12.3, -10, -99];
 num.sort();
 console.log(num);
+
