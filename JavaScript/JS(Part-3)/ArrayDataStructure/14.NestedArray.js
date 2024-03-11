18f1d78d7e3740b036913bd4d44016cdea9ccbbc
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-11 15:47:48 -0500

    added

---
+++ JavaScript/JS(Part-3)/ArrayDataStructure/14.NestedArray.js
@@ -0,0 +1,10 @@
+// Nested Array - also called Multi dimensional array.
+// array of arrays.
+
+let arr = [ ['rajia', 'rani'], ['chenchu', 'reddy','andhra'], ['roma','syal','jalandhar','punjab']];
+console.log(arr[0]);
+console.log(arr[0][0]);
+console.log(arr[1]);
+console.log(arr[1][0]);
+console.log(arr[2]);
+console.log(arr[2][0]);
