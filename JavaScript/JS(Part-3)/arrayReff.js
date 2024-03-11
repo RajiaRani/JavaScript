2a55a09546f8414cb44fddebd3d47828a259b829
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-11 15:30:59 -0500

    added

---
+++ JavaScript/JS(Part-3)/arrayReff.js
@@ -0,0 +1,14 @@
+let name = "rajia";
+let name1 = "rajia";
+console.log(name == name1); // return true
+console.log(name1 === name1); // return true
+
+// compare the array
+let arr = [];
+let arr1 = [];
+console.log("Compare empty array " ,arr === arr1);
+
+// Example 2
+let arr2 = [1,2,3,4];
+let arr3 = [1,2,3,4];
+console.log("Is arr2 & arr3 are equal : ", arr2 === arr3);
